be335d02717dd30a5fbcd7e8a1520c98129418c5
Rajesh Pathak <rrajeshpathak7@gmail.com>
2024-03-22 15:44:56 +0530

    initial commit

---
+++ src/test/java/com/org/bidkaro/config/BikaroConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.bidkaro.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikaroConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
